6cf3305c0ce9247b07acc2deafb6f1481ac39c10
FrancescMC <francescmc93@gmail.com>
2022-10-13 12:15:04 +0200

    trying to fix a bug

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ onMounted(async () => {
       router.push({ path: "/auth/login" });
     } else {
       // continue to dashboard
-      // router.push({ path: "/" });
+      router.push({ path: "/" });
     }
   } catch (e) {
     console.log(e);
